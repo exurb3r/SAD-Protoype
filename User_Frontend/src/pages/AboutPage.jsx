34bff9d3fb85ecf2aba6b1602fe1efd41facbf77
Roven <rovensantos03@gmail.com>
2026-02-17 00:00:48 +0800

    Fixing File Structure

---
+++ User_Frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react';
+
+function About(){
+    return(
+       <div>
+
+       </div>
+    )
+}
+
+export default About;
---
+++ User_Frontend/src/pages/Announcements.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react';
+
+function Announcements(){
+    return(
+       <div>
+
+       </div>
+    )
+}
+
+export default Announcements;
