5402aa95d0be095224a5ddc755266bd52494e9e4
Roven <rovensantos03@gmail.com>
2026-02-16 23:53:46 +0800

    Fixing File Structure

---
+++ Backend_and_Database/models/user_details/UserNotifications.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSubSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    description: {  
+        type: String,
+    }
+});
+
+const noteSchema = new Schema({
+    email:{
+        type: String,
+        required: true
+    }, 
+    notes: [noteSubSchema]
+});
+
+module.exports = mongoose.model('Task', noteSchema);
